86237abe427bee793f43cbf1ac242217816b5050
MohammadKaleaji <mohammadkaleaji@gmail.com>
2025-08-20 01:26:14 +0300

    working with routes

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,11 +5,12 @@ import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Home],
+  imports: [RouterOutlet, Header, ],
   template: `
     <app-header />
     <main>
-      <app-home />
+      <!-- <app-home /> -->
+       <router-outlet />
     </main>
   `,
   styles: [
